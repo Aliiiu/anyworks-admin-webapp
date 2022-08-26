cd23778217a2eed8b7c668c6bf0e69cf4544967f
aliiiu <ayokunumi99@gmail.com>
2022-08-26 20:40:43 +0100

    working

--- src/components/dashboard/DashboardSidebarHeader.tsx
+++ src/components/dashboard/DashboardSidebarHeader.tsx
@@ -1,56 +1,60 @@
-import React from 'react'
-import styled from 'styled-components'
-import { HamburgerMenu } from 'src/components/ui/HamburgerMenu'
-import clsx from 'clsx'
-import logo from 'src/assets/images/header/logo.svg'
+import React from 'react';
+import styled from 'styled-components';
+import { HamburgerMenu } from 'src/components/ui/HamburgerMenu';
+import clsx from 'clsx';
+import logo from 'src/assets/images/header/logo.svg';
 
 const DashboardSidebarHeaderContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-  width: 100%;
-  height: 5rem;
-  font-size: 18px;
-  font-weight: 500;
-  letter-spacing: 0.44px;
-  color: ${(props) => props.theme.colors.text_06};
-  padding: 8px 32px 8px 43px;
-  background-color: ${(props) => props.theme.colors.white};
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	gap: 10px;
+	width: 100%;
+	height: 5rem;
+	font-size: 18px;
+	font-weight: 500;
+	letter-spacing: 0.44px;
+	color: ${(props) => props.theme.colors.text_06};
+	padding: 8px 32px 8px 43px;
+	background-color: ${(props) => props.theme.colors.white};
 
-  .brand-logo {
-    width: 112px;
-  }
+	.brand-logo {
+		width: 112px;
+	}
 
-  @media (max-width: ${(props) => props.theme.breakpoint.md}) {
-    width: 100%;
-    height: 5rem;
-    border-bottom: 1px solid #eaeaea;
-  }
+	@media (max-width: ${(props) => props.theme.breakpoint.md}) {
+		width: 100%;
+		height: 5rem;
+		border-bottom: 1px solid #eaeaea;
+	}
 
-  &:not(.showOnDesktop) {
-    display: none;
+	&:not(.showOnDesktop) {
+		display: none;
 
-    @media (max-width: ${(props) => props.theme.breakpoint.md}) {
-      display: flex;
-    }
-  }
-`
+		@media (max-width: ${(props) => props.theme.breakpoint.md}) {
+			display: flex;
+		}
+	}
+`;
 interface Props {
-  isOpen: boolean
-  showOnDesktop?: boolean
-  toggleSidebar: Function
+	isOpen: boolean;
+	showOnDesktop?: boolean;
+	toggleSidebar: Function;
 }
 
-export const DashboardSidebarHeader  : React.FC<Props> =({ isOpen, toggleSidebar, showOnDesktop }) => {
+export const DashboardSidebarHeader: React.FC<Props> = ({
+	isOpen,
+	toggleSidebar,
+	showOnDesktop,
+}) => {
+	return (
+		<DashboardSidebarHeaderContainer
+			className={clsx('DashboardSidebarHeader', { showOnDesktop })}
+		>
+			<img className='brand-logo' src={logo} alt='anyworks' />
+			<HamburgerMenu isOpen={isOpen} onClick={toggleSidebar} />
+		</DashboardSidebarHeaderContainer>
+	);
+};
 
-
-  return (
-    <DashboardSidebarHeaderContainer className={clsx('DashboardSidebarHeader', { showOnDesktop })}>
-      <img className='brand-logo' src={logo} alt="anyworks" />
-      <HamburgerMenu isOpen={isOpen} onClick={toggleSidebar} />
-    </DashboardSidebarHeaderContainer>
-  )
-}
-
-export default DashboardSidebarHeader
+export default DashboardSidebarHeader;
---
+++ src/styles/Theme.tsx
@@ -0,0 +1,76 @@
+import React, { FC, ReactNode } from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+export const theme = {
+	colors: {
+		black: '#000000',
+		blue: '#219dd3',
+		red: '#EB5656',
+		text_01: '#333333',
+		gray_01: '#d9d9d9',
+		purple: '#7E00C4',
+		text_04: '#828282',
+		transparent: 'transparent',
+		white: '#ffffff',
+	},
+	breakpoint: {
+		lg: '1024px',
+		md: '768px',
+		sm: '425px',
+	},
+};
+
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+input,
+button {
+  padding: 0;
+  margin: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  scroll-behavior: smooth;
+  background-color: #F2F4F7;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+button {
+  border: 0;
+  background-color: transparent;
+}
+
+ul {
+  padding: 0;
+  margin: 0 0 0 1.1rem;
+  list-style-type: disc;
+  list-style-position: outside;
+}
+
+`;
+
+const Theme: FC<{ children: ReactNode }> = ({ children }) => (
+	<ThemeProvider theme={theme}>
+		<>
+			<GlobalStyle />
+			{children}
+		</>
+	</ThemeProvider>
+);
+
+export default Theme;
